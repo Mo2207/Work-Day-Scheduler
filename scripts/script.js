fe7eabcaa64cb2952874df076a0dea80602b8276
Gabe Morris <gabemorris1042@gmail.com>
2022-11-19 10:52:30 -0400

    added functionality for saving user input to localStorage and displaying it upon page refresh

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,4 @@
 
-// Display current time at top of page
-let headerTimeDisplay = $("#currentDay").html(dayjs().format("dddd, MMMM M"));
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -12,16 +6,19 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  $("#allTimeBlocks").on("click", ".saveBtn", function() {
+
+  // Event listener for every save button
+  $(".saveBtn").on("click", function() {
     let element = $(this);
-    // console.log($(this).attr("id"));
-    // console.log($(".description").val())
 
-    // Get the closest id and save its value as a key to localStorage
-    console.log(element.find("id"));
-    localStorage.setItem(element.find("id"), $(".description").val());
-    let userText = $(".description").val()
+    // Grab userInput object from localStorage, if it doesn't exist create the object
+    let userInput = JSON.parse(localStorage.getItem("userInput")||"{}");
 
+    // Assign the key (button that was pressed), and the value (selected textarea) to userInput
+    userInput[element.parent().attr("id")] = $(element.siblings(".description")).val();
+
+    // Save the newly created key/value to localStorage
+    localStorage.setItem("userInput", JSON.stringify(userInput));
   });
 
   // TODO: Add code to apply the past, present, or future class to each time
@@ -30,21 +27,23 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   
-  // console.log(dayjs().format('k'))
-  // if (dayjs().format("k") > dayjs().format("k")) {
-
-  // }
-  let timeBlockContainer = $(".container-fluid px-5")
-  for (let i=0; i < timeBlockContainer.length; i++) {
-    console.log("hello")
-  }
+ 
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  
+
+  // Iterate through all the input blocks keys/values
+  for (const [key, value] of Object.entries(JSON.parse(localStorage.getItem("userInput")))) {
+    // console.log(`${key}: ${value}`);
+
+    // Set the text of the current iteration's key, to equal the current iteration's value
+    $(`#${key}`).children(".description").text(value);
+  }
 
   // TODO: Add code to display the current date in the header of the page.
 
+  //
+  let headerTimeDisplay = $("#currentDay").html(dayjs().format("dddd, MMMM M"));
 
 });
